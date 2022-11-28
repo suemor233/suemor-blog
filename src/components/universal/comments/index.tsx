1fe1ce64151abd949d55b895114b58087f509a2a
suemor <3100825062@qq.com>
2022-11-28 22:33:21 +0800

    fix: comments dark mode

--- src/components/universal/comments/index.tsx
+++ src/components/universal/comments/index.tsx
@@ -1,8 +1,10 @@
 import type { FC } from 'react'
 
 import Giscus from '@giscus/react'
+import { useColorMode } from '../../../hooks/use-color-mode';
 
 export const Comments: FC<{ term: string }> = ({ term }) => {
+  const {isDark} = useColorMode()
   return (
     <Giscus
       repo="suemor233/suemor-blog"
@@ -14,7 +16,7 @@ export const Comments: FC<{ term: string }> = ({ term }) => {
       reactionsEnabled="1"
       emitMetadata="0"
       inputPosition="top"
-      theme="light"
+      theme={isDark ? 'dark' : 'light'}
       lang="zh-CN"
       loading="lazy"
     />
